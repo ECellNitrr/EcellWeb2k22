6b53fc7f86c6a2fca339863d20af127bdc1c8bbb
Nikhil-dev13 <nikhil13012000@gmail.com>
2021-01-31 20:36:13 +0530

    login signup modal for desktop

--- src/Components/Home/Sidebar/AuthModal.js
+++ src/Components/Home/Sidebar/AuthModal.js
@@ -10,22 +10,22 @@ const AuthModal = ({ closeModal, modalOpen }) => {
                 <div class={`container ${rightPanelActive}`} id="container">
                     <div class="form-container sign-up-container">
                         <form className="ls-form" action="#">
-                            <h1>Create Account</h1>
-                            <span>or use your email for registration</span>
+                            <h1 className="ls-heading">Create Account</h1>
+                            <span className="sub-text">or use your email for registration</span>
                             <input className="auth-input" type="text" placeholder="Name" />
                             <input className="auth-input" type="email" placeholder="Email" />
                             <input className="auth-input" type="password" placeholder="Password" />
-                            <button>Sign Up</button>
+                            <button className="ls-button">Sign Up</button>
                         </form>
                     </div>
                     <div class="form-container sign-in-container">
                         <form className="ls-form" action="#">
-                            <h1>Sign in</h1>
-                            <span>or use your account</span>
+                            <h1 className="ls-heading">Sign in</h1>
+                            <span className="sub-text">or use your account</span>
                             <input className="auth-input" type="email" placeholder="Email" />
                             <input className="auth-input" type="password" placeholder="Password" />
-                            <a href="#">Forgot your password?</a>
-                            <button>Sign In</button>
+                            <a className="ls-fpassword">Forgot your password?</a>
+                            <button className="ls-button">Sign In</button>
                         </form>
                     </div>
                     <div class="overlay-container">
@@ -35,12 +35,12 @@ const AuthModal = ({ closeModal, modalOpen }) => {
                                 <p>
                                     To keep connected with us please login with your personal info
                                     </p>
-                                <button onClick={(e) => {e.preventDefault(); setlogin(false)}} class="ghost" id="signIn">Sign In</button>
+                                <button onClick={(e) => {e.preventDefault(); setlogin(false)}} className="ghost ls-button" id="signIn">Sign In</button>
                             </div>
                             <div class="overlay-panel overlay-right">
                                 <h1>Hello, Friend!</h1>
                                 <p>Enter your personal details and start journey with us</p>
-                                <button onClick={(e) => {e.preventDefault(); setlogin(true)}} class="ghost" id="signUp">Sign Up</button>
+                                <button onClick={(e) => {e.preventDefault(); setlogin(true)}} className="ghost ls-button" id="signUp">Sign Up</button>
                             </div>
                         </div>
                     </div>
