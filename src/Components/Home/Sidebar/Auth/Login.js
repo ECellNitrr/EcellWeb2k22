4a0767316941935d23307c260c46681b0e6ef49e
Nikhil-dev13 <nikhil13012000@gmail.com>
2021-02-02 00:28:35 +0530

    init Login connect

--- src/Components/Home/Sidebar/Auth/Login.js
+++ src/Components/Home/Sidebar/Auth/Login.js
@@ -1,12 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = ({ setlogin, setForgotPassword }) => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState('');
+
+  const { email, password } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if(email.length < 1){
+        return
+    }
+    if(password.length < 1){
+        return
+    }
+    const verify_email = (email) => {
+        let re = /\S+@\S+\.\S+/;
+        return re.test(String(email).toLowerCase());
+    }
+    if(!verify_email(email)){
+        return
+    }
+    if(password.length<8){
+        return
+    }
+    //login(email, password);
+  };
+
   return (
-    <form className="ls-form" action="#">
+    <form className="ls-form" onSubmit={(e) => onSubmit(e)}>
       <div className="ls-heading">Sign in</div>
       <span className="sub-text">or use your account</span>
-      <input className="auth-input" type="email" placeholder="Email" />
-      <input className="auth-input" type="password" placeholder="Password" />
+      <input
+        className="auth-input"
+        type="email"
+        required
+        placeholder="Email"
+        name="email"
+        value={email}
+        onChange={(e) => onChange(e)}
+      />
+      <input
+        className="auth-input"
+        type="password"
+        placeholder="Password"
+        name="password"
+        value={password}
+        onChange={(e) => onChange(e)}
+        required
+      />
       <a
         onClick={(e) => {
           e.preventDefault();
