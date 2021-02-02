369dd17a33c807989fc6f34bc5a827fad5511111
rajesh6161 <rajeshranjan2712000@gmail.com>
2021-02-02 23:49:40 +0530

    commited

--- src/Components/Home/Sidebar/Sidebar.js
+++ src/Components/Home/Sidebar/Sidebar.js
@@ -1,92 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Logo from '../../../assets/logo-white.png';
 import './style.css';
 
-import Form from '../../Form/form';
-import OtpModal from '../../Form/otp';
-import LogoutModal from '../../Form/logout';
-import ForgetPass from '../../Form/forgetpass';
-import ChangePass from '../../Form/changepass';
-import CheckOtp from '../../Form/checkotp';
-
 import { NavLink, Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import * as actions from '../../../actions/authActions';
-import AuthModal from './AuthModal';
+
 const Sidebar = (props) => {
-  const [details, setDetails] = useState({
-    active: false,
-    forgetmail: '',
-    forgetOTP: '',
-  });
+  // to set navbar fixed or static
 
-  const { active, forgetmail, forgetOTP } = details;
-
-  const [modalOpen, setModalOpen] = useState(false);
-
-  const closeModal = () => setModalOpen(false);
-  const openModal = () => setModalOpen(true);
-
-  const setForgetMail = (mail) => {
-    setDetails({ forgetmail: mail });
-  };
-
-  const setForgetOTP = (otp) => {
-    setDetails({ forgetOTP: otp });
-  };
-
-  useEffect(() => {
-    document.onclick = (args) => {
-      if (args.target.id === 'dialog-target') {
-        console.log('called');
-      }
-    };
-  }, []);
-
-  const loggedout = (
-    <button
-      id="signup"
-      className="btn btn-outline-default"
-      data-toggle="modal"
-      data-target="#loginRegModal"
-      onClick={openModal}
-    >
-      Login/Sign Up
-    </button>
-  );
-
-  const loggedin = (
-    <button
-      id="signup"
-      className="btn btn-outline-default"
-      data-toggle="modal"
-      data-target="#logoutModal"
-      onClick={openModal}
-    >
-      {props.auth.first_name.toUpperCase()} {props.auth.last_name.toUpperCase()}
-    </button>
-  );
   return (
     <div>
-      <header>
+      <header style={{ position: props.position }}>
         <div className="leftBox">
           <img src={Logo} className="img-fluid" id="logo" />
         </div>
+
         {/* logged-in/logged-out part */}
         <div className="rightBox">
-          {props.auth.loggedin ? loggedin : loggedout}
+          <button id="signup" className="btn btn-outline-default">
+            Login/Sign Up
+          </button>
         </div>
       </header>
-      <div id="dialog-target">
-        <AuthModal modalOpen={modalOpen} closeModal={closeModal} />
-      </div>
       <div id="nav-container">
-        <div class="bg"></div>
-        <div class="button" tabindex="0">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
+        <div className="bg"></div>
+        <div className="button" tabindex="0">
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
         </div>
         <div id="nav-content" tabindex="0">
           <ul>
@@ -133,6 +73,4 @@ const Sidebar = (props) => {
   );
 };
 
-const mapStateToProps = (state) => state;
-
-export default connect(mapStateToProps, actions)(Sidebar);
+export default Sidebar;
