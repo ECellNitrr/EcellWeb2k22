8db33c83d6045c22372f574e1e8350956d078ac8
Nikhil-dev13 <nikhil13012000@gmail.com>
2021-02-02 22:28:33 +0530

    Init Login Modal

--- src/Components/Home/Sidebar/Auth/Login.js
+++ src/Components/Home/Sidebar/Auth/Login.js
@@ -6,7 +6,7 @@ const Login = ({ setlogin, setForgotPassword }) => {
     password: "",
   });
 
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   const { email, password } = formData;
 
@@ -15,27 +15,28 @@ const Login = ({ setlogin, setForgotPassword }) => {
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    if(email.length < 1){
-        return
+    if (email.length < 1) {
+      return;
     }
-    if(password.length < 1){
-        return
+    if (password.length < 1) {
+      return;
     }
     const verify_email = (email) => {
-        let re = /\S+@\S+\.\S+/;
-        return re.test(String(email).toLowerCase());
+      let re = /\S+@\S+\.\S+/;
+      return re.test(String(email).toLowerCase());
+    };
+    if (!verify_email(email)) {
+      return;
     }
-    if(!verify_email(email)){
-        return
-    }
-    if(password.length<8){
-        return
+    if (password.length < 8) {
+      return;
     }
     //login(email, password);
   };
 
   return (
     <form className="ls-form" onSubmit={(e) => onSubmit(e)}>
+      <div className="alert alert-danger network-error" role="alert">Network Error</div>
       <div className="ls-heading">Sign in</div>
       <span className="sub-text">or use your account</span>
       <input
@@ -47,6 +48,7 @@ const Login = ({ setlogin, setForgotPassword }) => {
         value={email}
         onChange={(e) => onChange(e)}
       />
+      <span className="error-text">Some Error text</span>
       <input
         className="auth-input"
         type="password"
@@ -56,6 +58,7 @@ const Login = ({ setlogin, setForgotPassword }) => {
         onChange={(e) => onChange(e)}
         required
       />
+      <span className="error-text">Some Error text</span>
       <a
         onClick={(e) => {
           e.preventDefault();
--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,6 +1,16 @@
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_ERROR } from './types'
+import faxios from '../axios';
+//Legacy Code
 export const updateUser = payload => dispatch => {
     dispatch({
         type: 'update_user',
         payload
     })
 } 
+//Reducers
+
+
+//Actions
+export const login = (email, password) => async dispatch => {
+    faxios.post();
+}
