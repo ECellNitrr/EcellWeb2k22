e9350661ad99e0e0d3dcc116221b531c5df3720c
Nikhil-dev13 <nikhil13012000@gmail.com>
2021-02-10 11:58:24 +0530

    resolve login bug

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,12 +1,29 @@
+import faxios from '../axios';
+
+//Type constants
 export const UPDATE_USER = 'UPDATE_USER'
 export const LOGOUT = 'LOGOUT'
-//Legacy Code
+export const GET_USER_DETAILS = 'GET_USER_DETAILS'
+
 export const updateUser = payload => dispatch => {
     dispatch({
         type: UPDATE_USER,
         payload
     })
+    dispatch(getUserDetails());
 } 
+
+const getUserDetails = () => async (dispatch) => {
+    try {
+        const { data } = await faxios().get('/users/get_user_details/');
+        dispatch({
+            type: GET_USER_DETAILS,
+            payload: data,
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 export const logout = () => dispatch => {
     dispatch({
