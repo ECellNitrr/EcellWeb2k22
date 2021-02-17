d6be94ecdfbff0646dc7a953b42988dc6798c899
rajesh6161 <rajeshranjan2712000@gmail.com>
2021-02-17 15:48:44 +0530

    this PR fixed all the invalid issues on main page

---
+++ src/Components/LeaderBoard/Item.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Item = () => {
+  return (
+    <div className="item">
+      <div className="rank">1</div>
+
+      <div className="name">Jhon Doe</div>
+      <div className="points">3,000 pts</div>
+    </div>
+  );
+};
+
+export default Item;
